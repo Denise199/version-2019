549e057deec6159ab2a3bb83809cb25ed0726dd3
Denise199 <x.fernandez@alumno.um.edu.ar>
2019-09-12 18:59:45 -0300

    JUEGO

---
+++ 58046-Fernandez, Ximena Denise/Clase 06/Juego.py
@@ -0,0 +1,31 @@
+from generador import generador_mejor, generador
+adivinado=False
+minimo=0
+maximo=100
+aleatorio=generador(0, 100)
+while  adivinado == False:
+    print("Ingrese un numero")
+    numero=int(input())
+    if numero == aleatorio:
+        print("Ganaste")
+        adivinado=True
+    if numero < aleatorio:
+        print("El numero es mayor!!!")
+    if numero > aleatorio:
+        print("El numero es menor")
+    if adivinado == False:
+        print("Ahora le toca a la computadora")
+        computadora=generador(minimo, maximo)
+        print("La computadora penso: " + str(computadora))
+        if computadora == aleatorio:
+            print("Gano la computadora")
+            adivinado=True
+        if computadora < aleatorio:
+            print("Computadora es MAYOR")
+            minimo=computadora
+        if computadora > aleatorio:
+            print("Computadora es MENOR")
+            maximo=computadora
+        
+        
+        
--- 58046-Fernandez, Ximena Denise/Clase 06/generador.py
+++ 58046-Fernandez, Ximena Denise/Clase 06/generador.py
@@ -2,7 +2,16 @@ import random
 def generador(minimo, maximo):
     return random.randint(minimo, maximo)
 if __name__ == "__main__":
-    print(generador(1,20))
+    print(generador(1,30))
 #a=generador(1,100)
 #b=generador(5,10)
-#print(str(a)+" "+str(b))
+#print(str(a)+" "+str(b))}
+def generador_mejor(minimo, maximo, lista):
+    encontrado=True
+    while encontrado:
+        aleatorio=random.randint(minimo, maximo)
+        encontrado=False
+        if aleatorio in lista:
+            encontrado=True
+    return aleatorio
+#print(generador_mejor(3, 13, [9, 13]))
