5fdba39ad679e3a4a014341198cdf328df15611b
Denise199 <x.fernandez@alumno.um.edu.ar>
2019-09-26 18:59:18 -0300

    tateti

--- 58046-Fernandez, Ximena Denise/Clase 07/tateti.py
+++ 58046-Fernandez, Ximena Denise/Clase 07/tateti.py
@@ -1,2 +1,36 @@
-from tablero import draw_tablero
-tateti=False
+from tablero import draw_tablero, win, validate, full
+tablero=[ ]
+for i in range(0,9):
+    tablero.append(" ")
+draw_tablero(tablero)
+while win(tablero) == False and full(tablero) == False:
+    print("Turno de la X")
+    valido=False
+    while not valido:
+        print("Ingrese una posicion valida de 1 a 9 (X) ")
+        posicion=int(input())
+        valido= validate(tablero, posicion)
+        if not valido:
+            print("Error de posicion")
+    tablero[posicion-1]="X"
+    draw_tablero(tablero)
+    gano=win(tablero)
+    if gano:
+        print("Gano X")
+    else:
+        print("Turno de la O")
+        valido=False
+        while not valido:
+            print("Ingrese una posicion valida de 1 a 9 (O) ")
+            posicion=int(input())
+            valido= validate(tablero, posicion)
+            if not valido:
+                print("Error de posicion")
+        tablero[posicion-1]="O"
+        draw_tablero(tablero)
+        gano=win(tablero)
+        if gano:
+            print("Gano O")
+if full(tablero) and not win(tablero):
+    print("Nadie gano")
+
